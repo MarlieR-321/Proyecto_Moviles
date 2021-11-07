e95ecd23f8dd7f2ffa9037102dc52823b510389c
Gerald Diaz <gerald.diaz11715@est.uca.edu.ni>
2021-11-06 22:20:54 -0600

    Prototipos de layout Gerald

---
+++ app/src/main/java/ni/edu/uca/proyecto_final/Pantalla_de_Ejercicios.kt
@@ -0,0 +1,11 @@
+package ni.edu.uca.proyecto_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantalla_de_Ejercicios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_de_ejercicios)
+    }
+}
