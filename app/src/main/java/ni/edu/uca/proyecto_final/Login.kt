eb8ea2a2b2b9e0e6956f565d906833cd7376b865
MarlieR-321 <marlie.ramirez12169@est.uca.edu.ni>
2021-11-07 09:00:21 -0600

    Union de Catherine

---
+++ app/src/main/java/ni/edu/uca/proyecto_final/Login.kt
@@ -0,0 +1,11 @@
+package ni.edu.uca.proyecto_final
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Login: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/ni/edu/uca/proyecto_final/Registrar_Cuenta.kt
@@ -0,0 +1,11 @@
+package ni.edu.uca.proyecto_final
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Registrar_Cuenta: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar)
+    }
+}
